a81d3fef9ffbee62b461274973ac09daa4b00b31
UnwraperMan <unwraperman@gmail.com>
2022-11-01 16:41:23 +0300

    Добавлены: Label значение счетчика.                      Кнопка, при нажатии которой значение счетчика увеличивается на единицу.

--- Practicum-Sprint-3-Counter/ViewController.swift
+++ Practicum-Sprint-3-Counter/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var touches = 0
+    
+    @IBOutlet weak var counterLable: UILabel!
+    
+    @IBAction func counterButton(_ sender: Any) {
+        touches += 1
+        counterLable.text = "Значение счетчика: \(touches)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
