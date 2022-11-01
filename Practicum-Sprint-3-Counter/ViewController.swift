a921f9e9be219eadee86d729273962f557744cec
UnwraperMan <unwraperman@gmail.com>
2022-11-01 15:05:31 +0300

    Initial Commit

---
+++ Practicum-Sprint-3-Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practicum-Sprint-3-Counter
+//
+//  Created by ARTEM on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
